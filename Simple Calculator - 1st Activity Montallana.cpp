f9ff184680881c9fd62d168324bb16855a697203
kit <keithwilliam.montallana@live.ama.edu.ph>
2025-09-05 21:18:14 +0800

    Add files via upload

---
+++ Simple Calculator - 1st Activity Montallana.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+    /*Keith William Montallana
+    25000926810*/
+int main()
+{
+    string name;
+    int num1, num2, sum;
+    
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello, " << name << "! Let's add two numbers." << endl;
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "." << endl;
+    return 0;
+
+    /*Keith William Montallana
+    25000926810*/
+}
